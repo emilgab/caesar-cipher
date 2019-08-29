241e10fc7cbe7310325f677dd1d22c37d030a754
Emil Gabrielli <emilgabrielli@gmail.com>
2019-08-29 11:25:29 +0200

    Added comments in the caesar cipher object

--- caesar.py
+++ caesar.py
@@ -1,3 +1,4 @@
+# imports the string module for getting alphabet
 import string
 
 class CaesarCipher():
@@ -6,15 +7,18 @@ class CaesarCipher():
     
     def __init__(self, seed):
         self.seed = seed
+        # constructs a ciphered alphabet from the seed set by the user
         self.cipher_alphabet = self.alphabet[self.seed:] + self.alphabet[0:self.seed]
     
     def encrypt(self, message):
+        # goes through the message and encrypts it using the index position of each letter in the original alphabet.
         encrypted_msg = ""
         for char in message:
             encrypted_msg += self.cipher_alphabet[(self.alphabet.index(char))]
         return encrypted_msg
     
     def decrypt(self, message):
+        # reverses the process of encryption by using the ciphered alphabet index in the original alphabet.
         decrypted_msg = ""
         for char in message:
             decrypted_msg += self.alphabet[(self.cipher_alphabet.index(char))]
